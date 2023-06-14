8b9fef3797855ff2666cb124a42126d866be81f5
Shuichi <zhuravkov.mai@gmail.com>
2023-06-15 00:10:41 +0300

    added api

---
+++ content_delivery/src/api/v1/images.py
@@ -0,0 +1,36 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Query, Request
+from fastapi.responses import HTMLResponse
+from schemas.images import ImageSchema
+from services.images import ImageService, get_film_service
+from core.config import templates
+
+router = APIRouter()
+
+
+@router.get(
+    "/",
+    response_model=ImageSchema,
+    summary="Получение изображения"
+)
+async def get_image(
+    category: Annotated[list[str] | None, Query()] = None,
+    image_service: ImageService = Depends(get_film_service)
+):
+    return await image_service.get_image(categories=category)
+
+@router.get(
+    "/html",
+    response_class=HTMLResponse
+)
+async def get_image_html(
+    request: Request,
+    category: Annotated[list[str] | None, Query()] = None,
+    image_service: ImageService = Depends(get_film_service)
+):
+    image = await image_service.get_image(categories=category)
+    return templates.TemplateResponse(
+        "image.html",
+        {"request": request, "image": image}
+    )
